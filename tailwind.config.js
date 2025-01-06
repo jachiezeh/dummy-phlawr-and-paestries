336d52001ce25d5ed31d80112a24f61d3abf189b
Jachi Ezeh <jachukwummaezeh@gmail.com>
2025-01-06 13:21:49 +0100

    Initial commit for Tailwind test

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./public/**/*.{html,js}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#E2E2D5',
+          200: '#DA0037',
+          300: '#A5BECC',
+          400: '#243A73'
+        },
+        secondary: {
+          100: '#171717',
+          200: '#EDEDED',
+        }
+      },
+      fontFamily:{
+        body:['Open Sans'],
+        header:['Merriweather']
+      }
+    },
+  },
+  plugins: [],
+}
+
